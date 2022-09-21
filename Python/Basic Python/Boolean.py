21620e38583e3d3c7af303fe4ef14b31a9a28252
Tontan-Hak <tontanhak97@gmail.com>
2022-09-21 23:18:25 +0700

    Add boolean

---
+++ Python/Basic Python/Boolean.py
@@ -0,0 +1,9 @@
+# Booleans and if statements
+light_is_on = True
+if light_is_on: 
+    print("The light is on!")
+
+# Find something around you that could be represented by a Boolean and make variable for it
+Computer_is_open = False
+if Computer_is_open:
+    print("The computer is open!")
