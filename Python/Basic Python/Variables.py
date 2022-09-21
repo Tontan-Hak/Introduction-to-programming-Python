3f786e7b0f9c7b439d4979d042670da53b073c75
Tontan-Hak <tontanhak97@gmail.com>
2022-09-21 23:08:20 +0700

    add new lesson about string, and variable

---
+++ Python/Basic Python/Variables.py
@@ -0,0 +1,13 @@
+# Using variables in string
+from calendar import day_name
+
+
+day = 3 
+month = "October"
+temp = 65 
+
+print(f"Today is {month} {day} and it 's {temp} degrees ourside")
+
+# Make a variable called day_name and set it to day of the week like Tuesday and add it to the string we print
+day_name = "Wednesday" 
+print(f"Day of the week is {day_name}.")
---
+++ Python/Basic Python/string.py
@@ -0,0 +1,12 @@
+age =32 
+weight = 190.4
+
+shirt = "blue"
+print(shirt)
+
+store = "Gimey's store"
+print(store)
+
+# Put the name of a movie you like into a variable called movie
+movie = "Avatar"
+print(movie)
