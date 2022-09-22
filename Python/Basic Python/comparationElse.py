f55bf75a39033dbf03f85ef33bee9ad34ad83c00
Tontan-Hak <tontanhak97@gmail.com>
2022-09-22 23:35:53 +0700

    add comparation and else

---
+++ Python/Basic Python/comparationElse.py
@@ -0,0 +1,17 @@
+# Comparation and else
+light_is_on = True
+if light_is_on:
+    print("The light is on!")
+else:
+    print("We are in the dark")
+
+Weight = 190.4 
+if Weight < 195: 
+    print("You are under weight :)")
+
+# Make a variable age, and if someone is 18 years of older print that they are an adult. Otherwise print that they are a child.
+age = 32 
+if age != 18: 
+    print("They are an adult.")
+else: 
+    print("The are a child.")
