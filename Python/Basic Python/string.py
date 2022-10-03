1601949c1b5ec4dcc7ef825040cfa3d496e1ee14
Tontan-Hak <tontanhak97@gmail.com>
2022-10-03 15:49:24 +0700

    Add string code

--- Python/Basic Python/string.py
+++ Python/Basic Python/string.py
@@ -10,3 +10,16 @@ print(store)
 # Put the name of a movie you like into a variable called movie
 movie = "Avatar"
 print(movie)
+
+# Print only if "free" is present:
+txt = "The best things in life are free!"
+if "free" in txt:
+  print("Yes, 'free' is present.")
+
+  # Check the free in the following question:
+  txt = "The best things in life are free!"
+print("expensive" not in txt)
+# 
+txt = "The best things in life are free!"
+if "expensive" not in txt:
+  print("No, 'expensive' is NOT present.")
