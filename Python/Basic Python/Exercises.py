e783d626a994a59d097a9da78904c3a128f39204
Tontan-Hak <tontanhak97@gmail.com>
2022-10-03 14:26:05 +0700

    Add exercis code

---
+++ Python/Basic Python/Exercises.py
@@ -0,0 +1,20 @@
+# The following code example would print the data type of x, what data type would that be?
+x = ["apple", "banana", "cherry"]
+print(type(x))
+# ->list
+
+# The following code example would print the data type of x, what data type would that be?
+x = ("apple", "banana", "cherry")
+print(type(x))
+# -> tuple
+
+# The following code example would print the data type of x, what data type would that be?
+x = {"name" : "John", "age" : 36}
+print(type(x))
+# -> dict
+
+# The following code example would print the data type of x, what data type would that be?
+x = True
+print(type(x))
+# -> bool
+
