01f75732e8d257dc79b647f841dbab069d830e1c
Tontan-Hak <tontanhak97@gmail.com>
2022-10-03 14:04:37 +0700

    add code

--- Python/Basic Python/Globle_variable.py
+++ Python/Basic Python/Globle_variable.py
@@ -8,3 +8,21 @@ def myfunc():
 myfunc()
 
 print("Python is " + x)
+
+# Global Variable 
+x = "Awesome"
+def myfunc():
+    x = "Fantastic"
+    print("Python is " +x)
+
+myfunc()
+print("Python is " +x)
+
+# The global keyword
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+
+print("Python is " + x)
