c26069eb3fb774c1d4c3ddd5cb67d54078b8767a
Tontan-Hak <tontanhak97@gmail.com>
2022-10-04 13:58:58 +0700

    Add code

--- Python/Python for non programmers/Lists, loop and Dictionaries/Boolean.py
+++ Python/Python for non programmers/Lists, loop and Dictionaries/Boolean.py
@@ -6,3 +6,17 @@ if b > a:
   print("b is greater than a")
 else:
   print("b is not greater than a")
+
+# Boolean
+print(bool("abc"))
+print(bool(123))
+print(bool(["apple", "cherry", "banana"]))
+
+# Some Values are False
+print(bool(False))
+print(bool(None))
+print(bool(0))
+print(bool(""))
+print(bool(()))
+print(bool([]))
+print(bool({}))
