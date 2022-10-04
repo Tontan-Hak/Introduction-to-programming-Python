9f2a03961b90b4964eb5f3325aca02b630187b9b
Tontan-Hak <tontanhak97@gmail.com>
2022-10-04 09:03:11 +0700

    Add replace string code

--- Python/Python for non programmers/Lists, loop and Dictionaries/lists.py
+++ Python/Python for non programmers/Lists, loop and Dictionaries/lists.py
@@ -3,3 +3,19 @@ print(fav_movies[6])
 # Make the favorite numbers
 fav_numbers = [3,8,12]
 print(fav_numbers[2])
+
+# Upper case
+A = "hello world"
+print(A.upper())
+
+# Lower Case
+a = "Hello crush"
+print(a.lower())
+
+# Remove Whitespace
+b = " I love you! "
+print(b.strip()) # return "I love you!"
+
+# Replace string
+c = "Tontan Hak"
+print(c.replace("Ton", "Ly"))
