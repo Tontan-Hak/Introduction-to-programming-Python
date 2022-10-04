596d926368b91fb40fdb652ebff91dfb3913680c
Tontan-Hak <tontanhak97@gmail.com>
2022-10-04 09:55:57 +0700

    add string format code

--- Python/Python for non programmers/Lists, loop and Dictionaries/lists.py
+++ Python/Python for non programmers/Lists, loop and Dictionaries/lists.py
@@ -19,3 +19,33 @@ print(b.strip()) # return "I love you!"
 # Replace string
 c = "Tontan Hak"
 print(c.replace("Ton", "Ly"))
+
+# Split string
+d = "Hello, World!"
+e = d.split(",")
+print(e)
+
+# String Cancatenation
+f = "Lytan"
+g = "Hak"
+h = f + " " + g
+print(h)
+
+# String format 
+age = 24
+txt = "My name is Tontan, and I am {} years old"
+print(txt.format(age))
+
+# 
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want {} pieces of item {} for {} dollars."
+print(myorder.format(quantity, itemno, price))
+
+# Use {0}
+quantity = 3
+itemno = 567
+price = 49.95
+myorder = "I want to pay {2} dollars for {0} pieces of item {1}."
+print(myorder.format(quantity, itemno, price))
