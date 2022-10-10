472ffc287560d2e81b5d21fe7c78ba94406aa93a
Tontan-Hak <tontanhak97@gmail.com>
2022-10-10 13:57:44 +0700

    Add list code

--- Python/Basic Python/List.py
+++ Python/Basic Python/List.py
@@ -1,3 +1,26 @@
 # Create a lists
 thislist = ["apple", "banana", "cherry"]
 print(thislist)
+
+# List Length
+thislist = ["apple", "banana", "cherry"]
+print(len(thislist))
+
+# List Item Data-Type
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+print(list1)
+print(list2)
+print(list3)
+
+# 
+list1 = ["abc", 34, True, 40, "male"]
+
+print(list1)
+
+# Type
+mylist = ["apple", "banana", "cherry"]
+
+print(type(mylist))
