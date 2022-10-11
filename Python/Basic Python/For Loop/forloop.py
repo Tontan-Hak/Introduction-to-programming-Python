2fa3bddba842fb741b8b13e03682ffc42b7d6f7f
Tontan-Hak <tontanhak97@gmail.com>
2022-10-11 21:09:27 +0700

    add for loop

--- Python/Basic Python/For Loop/forloop.py
+++ Python/Basic Python/For Loop/forloop.py
@@ -1,3 +1,10 @@
 # For Loop
-for number in range(10):
-    print("Hello")
+for number in range(3):
+    print(number)
+fav_movie = ["Iron man", "Sad man", "Joker"]
+for movie in fav_movie:
+    print(movie)
+# Loop 40 time and print the number of the loop time 2 . Ex. 2,4,6,8
+for number1 in range(40): 
+    number1  = 2 * (number1 + 1)
+    print(number1)
