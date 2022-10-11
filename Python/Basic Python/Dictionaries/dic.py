11081a10ecc8f3bc31221a3aec3c47e7de610fa2
Tontan-Hak <tontanhak97@gmail.com>
2022-10-11 21:22:02 +0700

    add dictionaries code

---
+++ Python/Basic Python/Dictionaries/dic.py
@@ -0,0 +1,10 @@
+# Dictionaries
+cats = {"Jane":6 , "Tom":23, "Sara":24}
+cats["Chimey"] = 2
+del(cats["Sara"])
+len(cats)
+print(len(cats))
+
+# Create a dictionary with Ints for key and Booleans for values
+Ints_and_Bool = {5:True, 7:False, 144:False}
+print(Ints_and_Bool)
