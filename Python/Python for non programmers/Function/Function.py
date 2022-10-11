3f3659e30e4a61e7c946e033c55007c4ad0f6355
Tontan-Hak <tontanhak97@gmail.com>
2022-10-11 22:56:01 +0700

    add parameter

--- Python/Python for non programmers/Function/Function.py
+++ Python/Python for non programmers/Function/Function.py
@@ -1,3 +1,11 @@
 # Functions
 def bark():
     print("woof soof")
+    print("I am a cat!")
+for x in range(4):
+    bark()
+
+# Create a function called hello that prints "Hello Crush"
+def hello():
+    print("Hello Crush")
+hello()
