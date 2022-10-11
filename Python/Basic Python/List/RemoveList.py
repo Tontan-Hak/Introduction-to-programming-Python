c6e5202faadde0c4cb27d114507189ab8fc65f92
Tontan-Hak <tontanhak97@gmail.com>
2022-10-11 20:42:09 +0700

    add favorite number on the list

--- Python/Basic Python/List/RemoveList.py
+++ Python/Basic Python/List/RemoveList.py
@@ -7,3 +7,11 @@ print(F)
 A = ["Table", "Chair", "Salong"]
 A.pop(2)
 print(A)
+
+# Favorite movie
+fav_movie = ["Sad man", "Ant man", "Hulk"]
+print(fav_movie[1])
+
+# Make the list of your favorite numbers and print the fist number
+fav_number = ["3", "8", "17"]
+print(fav_number[0])
