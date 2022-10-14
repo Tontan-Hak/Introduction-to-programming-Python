3709347c83dc40a37dc4690cc3c2708a48d17593
Tontan-Hak <tontanhak97@gmail.com>
2022-10-14 14:19:57 +0700

    add loop tuple

--- Python/Basic Python/Tuple/TypleLength.py
+++ Python/Basic Python/Tuple/TypleLength.py
@@ -17,3 +17,37 @@ print(type(thistuple))
 # Access Tuple
 Fruits = ("apple", "banana","cherry")
 print(Fruits[2])
+
+#This will return the items from position 2 to 5.
+#Remember that the first item is position 0,
+#and note that the item in position 5 is NOT included
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:5])
+
+# By leaving out the end value, the range will go on to the end of the list:
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[2:])
+
+#Negative indexing means starting from the end of the tuple.
+#This example returns the items from index -4 (included) to index -1 (excluded)
+#Remember that the last item has the index -1,
+thistuple = ("apple", "banana", "cherry", "orange", "kiwi", "melon", "mango")
+print(thistuple[-4:-1])
+
+# Convert the tuple into a list to be able to change it:
+x =("apple", "banana","cherry")
+y = list(x)
+y[1] = "kiwi"
+x = tuple(y)
+print(x)
+
+# Using loop throught tuple
+fruit = ("apple","banana", "cherry")
+for x in fruit:
+    print(x)
+
+# Join the two tuple
+tuple1 = ("a", "b", "c")
+tuple2 = (1,2,3)
+tuple3 = tuple1 + tuple2
+print(tuple3)
