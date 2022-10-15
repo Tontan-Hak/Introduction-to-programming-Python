8437baddbc1ab40ed776d8e0302f007e761741e5
Tontan-Hak <tontanhak97@gmail.com>
2022-10-15 12:24:09 +0700

    Add code dict

--- Python/Basic Python/Dictionary/Dic.py
+++ Python/Basic Python/Dictionary/Dic.py
@@ -60,3 +60,57 @@ thisdict = {
 thisdict.update({"color": "red"})
 
 print(thisdict)
+
+# Remove dictionary 
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+
+# Loop 
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(x)
+
+# or Loop
+thisdict =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
+
+# Copy 
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+
+# Create a dictionary that contain three dictionaries:
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+
+print(myfamily)
