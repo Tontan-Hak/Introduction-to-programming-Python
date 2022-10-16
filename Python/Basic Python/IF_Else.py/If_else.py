92141544dcd9a0839f0f7f869b3256551ffd3a7e
Tontan-Hak <tontanhak97@gmail.com>
2022-10-16 09:15:33 +0700

    add code

--- Python/Basic Python/IF_Else.py/If_else.py
+++ Python/Basic Python/IF_Else.py/If_else.py
@@ -44,3 +44,13 @@ b = 33
 c = 5000
 if a > b or a > c: 
   print("At least one if the condition is true")
+
+# Nested if  
+x = 41
+
+if x > 10:
+  print("Above ten,")
+  if x > 20:
+    print("and also above 20!")
+  else:
+    print("but not above 20.")
