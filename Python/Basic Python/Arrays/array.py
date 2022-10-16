45dcd2528584d3cf4d12ce44f2464e05071c8185
Tontan-Hak <tontanhak97@gmail.com>
2022-10-16 18:13:50 +0700

    add arrays

---
+++ Python/Basic Python/Arrays/array.py
@@ -0,0 +1,12 @@
+# Print each item in the cars array:
+cars = ["Ford", "Volvo", "BMW"]
+
+for x in cars:
+  print(x)
+
+# Adding array element
+cars = ["Ford", "Volvo", "BMW"]
+
+cars.append("Honda")
+
+print(cars)
