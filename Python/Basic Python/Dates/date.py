12b7b5e78813fb4e5373a637bbf50d8f664e048d
Tontan-Hak <tontanhak97@gmail.com>
2022-10-17 22:06:56 +0700

    add math

---
+++ Python/Basic Python/Dates/date.py
@@ -0,0 +1,20 @@
+# Import the datetime module and display the current date:
+import datetime
+
+x = datetime.datetime.now()
+print(x)
+
+# Date Output
+import datetime
+
+x = datetime.datetime.now()
+
+print(x.year)
+print(x.strftime("%A"))
+
+# Create a date object:
+import datetime
+
+x = datetime.datetime(2020, 5, 17)
+
+print(x)
---
+++ Python/Basic Python/Math/math.py
@@ -0,0 +1,24 @@
+# Return the value of 4 to the power of 3 (same as 4 * 4 * 4):
+x = pow(4, 3)
+print(x)
+
+# squaire
+import math
+
+x = math.sqrt(64)
+print(x)
+
+# Rounds up
+import math
+
+x = math.ceil(1.7)
+y = math.floor(1.4)
+
+print(x) # returns 2
+print(y) # returns 1
+
+# 
+import math
+
+x = math.pi
+print(x)
