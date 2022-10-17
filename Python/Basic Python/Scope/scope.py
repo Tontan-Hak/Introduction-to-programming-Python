8f8f998692781f964742ce5882e040fff3aa4fe4
Tontan-Hak <tontanhak97@gmail.com>
2022-10-17 20:38:03 +0700

    add scope code

--- Python/Basic Python/Scope/scope.py
+++ Python/Basic Python/Scope/scope.py
@@ -4,3 +4,22 @@ def myfunc():
   print(x)
 
 myfunc()
+
+# The local variable can be accessed from a function within the function:
+def myfunc():
+  x = 300
+  def myinnerfunc():
+    print(x)
+  myinnerfunc()
+
+myfunc()
+
+# A variable created outside of a function is global and can be used by anyone:
+x = 300
+
+def myfunc():
+  print(x)
+
+myfunc()
+
+print(x)
