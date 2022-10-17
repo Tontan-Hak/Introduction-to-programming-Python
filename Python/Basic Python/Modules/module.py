45578a5e92f49173ab67a865fc5ad395ebc07eac
Tontan-Hak <tontanhak97@gmail.com>
2022-10-17 21:11:42 +0700

    add module code

---
+++ Python/Basic Python/Modules/module.py
@@ -0,0 +1,16 @@
+# Import the module named mymodule, and call the greeting function:
+import mymodule
+mymodule.greeting("Jonathan")
+
+# Variables in Module
+person1 = {
+  "name": "John",
+  "age": 36,
+  "country": "Norway"
+}
+
+# 
+import mymodule
+
+a = mymodule.person1["age"]
+print(a)
