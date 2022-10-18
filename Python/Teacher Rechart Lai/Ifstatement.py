8263f5fc4b47cf0093fa221dd429290feab92e5f
Tontan-Hak <tontanhak97@gmail.com>
2022-10-18 13:15:55 +0700

    add if and ellse statement

---
+++ Python/Teacher Rechart Lai/Ifstatement.py
@@ -0,0 +1,36 @@
+# Example 
+x = int(input("Entter th number: "))
+if x < 10 : 
+    print('Smaller')
+else: 
+    print('Bigger')
+print('Finish')
+
+# Grade
+mark = int(input("Enter your marks: "))
+
+if mark >= 80:
+    	print("Congratulations, you have scored an A!")
+
+elif mark >= 70:
+    	print("Congratulations, you have scored a B!")
+
+elif mark >= 60:
+    	print("Congratulations, you have scored a C!")
+
+elif mark >= 50:
+    	print("Congratulations, you have scored a D!")
+else:
+    	print("You have scored an N!")
+print("Unfortunately, you did not pass!")
+
+# 
+citizen = input("Are you a citizen of Australia (y/n)? ")
+if citizen == 'n':
+    	print("You are not eligible to vote.")
+else:
+    	age = int(input("How old are you (on your last birthday)? "))
+    	if age < 18:
+        		print("You are too young to vote")
+    	else:
+        		print("You are eligible to vote")
